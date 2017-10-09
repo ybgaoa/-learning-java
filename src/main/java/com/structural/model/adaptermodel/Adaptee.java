d7fddbb15cd87f6825d81261f2b03f3d92ddcfe0
ybgaoa <ybgaoa@isoftstone.com>
2017-10-09 16:38:30 +0800

    适配器模式

---
+++ src/main/java/com/structural/model/adaptermodel/Adaptee.java
@@ -0,0 +1,7 @@
+package com.structural.model.adaptermodel;
+/*源(Adapee)角色：现在需要适配的接口。*/
+public class Adaptee {
+	public void sampleOperation1(){
+		
+	}
+}
---
+++ src/main/java/com/structural/model/adaptermodel/Target.java
@@ -0,0 +1,13 @@
+package com.structural.model.adaptermodel;
+/*目标(Target)角色：这就是所期待得到的接口。
+ * 注意：由于这里讨论的是类适配器模式，因此目标不可以是类。*/
+public interface Target {
+	/**
+     * 这是源类Adaptee也有的方法
+     */
+    public void sampleOperation1(); 
+    /**
+     * 这是源类Adapteee没有的方法
+     */
+    public void sampleOperation2(); 
+}
