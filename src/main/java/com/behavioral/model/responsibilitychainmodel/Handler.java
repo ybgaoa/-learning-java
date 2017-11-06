b51c99fd2ddf1706c353a2e52249315f1f91dd3b
ybgaoa <ybgaoa@isoftstone.com>
2017-11-06 09:59:42 +0800

    责任链模式

---
+++ src/main/java/com/behavioral/model/responsibilitychainmodel/Handler.java
@@ -0,0 +1,29 @@
+package com.behavioral.model.responsibilitychainmodel;
+/*抽象处理者(Handler)角色：定义出一个处理请求的接口。
+ * 如果需要，接口可以定义 出一个方法以设定和返回对下家的引用。
+ * 这个角色通常由一个Java抽象类或者Java接口实现。*/
+public abstract class Handler {
+	/**
+     * 持有下一个处理请求的对象
+     */
+    protected Handler successor = null;
+    /**
+     * 取值方法
+     */
+    public Handler getSuccessor() {
+        return successor;
+    }
+    /**
+     * 设置下一个处理请求的对象
+     */
+    public void setSuccessor(Handler successor) {
+        this.successor = successor;
+    }
+    /**
+     * 处理聚餐费用的申请
+     * @param user    申请人
+     * @param fee    申请的钱数
+     * @return        成功或失败的具体通知
+     */
+    public abstract String handleFeeRequest(String user , double fee);
+}
